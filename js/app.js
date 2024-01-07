a1941797de82e7b210d707a90a81d38017a2fde9
Kartikey Mishra <mishrakartikey805@gmail.com>
2024-01-07 20:08:33 +0530

    initial

--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
 function run(){
+    const inputField = document.querySelector(".inputField"); 
+    hide = document.getElementById('hide').classList.toggle('hide')
+    output = document.getElementById('solve')
     about = document.getElementById('about').value
-    const inputField = document.getElementById("inputfield"); 
-    hide = document.getElementById('info')
-    hide.classList.toggle('hide')
-    output = document.getElementById('output-container')
+    academic = document.getElementById('academic').value
+    project = document.getElementById('project').value
     output.innerHTML = `
-    <div class="output-info">
-            <img src="./img/ex.jpeg" alt="">
-            <h1>Name</h1>
+    <div class="output-container" id="output-container">
+    <div class="output-left">
+            <h1>NAME</h1>
             <p>${inputField["name"].value}</p>
-            <h3>Email</h3>
-            <a href="/">${inputField["email"].value}</a>
-            <h3>Phone No.</h3>
-            <p>${inputField["number"].value}</p>
+            <h1>ABOUT</h1>
+            <p>${about}</p>
+            <h1>SKILLS</h1>
+            <p>${inputField["skills"].value}</p>
+            <h1>FUN FACT</h1>
+            <p>${inputField["fact"].value}</p>
+            <h1>GITHUB</h1>
+            <a href="${inputField["giturl"].value}">
+                Github
+            </a>
         </div>
-        <div class="output-role">
-        <h2>Role</h2>
-        <p>${inputField["role"].value}</p>
-        <h2>Skills</h2>
-        <p>${inputField["skills"].value}</p>
-        <h2>About</h2>
-        <p>${about}</p>
-    </div>
+        <div class="output-right">
+            <h1>WORK EXPEIENCE</h1>
+            <p>${inputField["work"].value}</p>
+            <h1>ACADEMIC</h1>
+            <p>${academic}
+                </p>
+                <h1>PROJECT</h1>
+                <p>${project}</p>
+                    <h1>LINKEDIN</h1>
+                    <a href="${inputField["linkurl"].value}">LINKEDIN</a>
+        </div>
+        <div class='printbtn'>
+        <button onclick='print()'>Print</button>
+        </div>
+        </div>
     `
 }
